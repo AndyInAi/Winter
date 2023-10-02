467e99851b8a6ebd66adf8d1ad821a7437e20f6b
AndyInAi <128768915+AndyInAi@users.noreply.github.com>
2023-10-02 14:19:30 +0800

    更新 Kafka 访问类

--- src/winter/Kafka.java
+++ src/winter/Kafka.java
@@ -1,8 +1,12 @@
 
 package winter;
 
+import static java.util.Collections.singleton;
+
 import java.sql.Timestamp;
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
@@ -13,12 +17,20 @@ import java.util.stream.Collectors;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.TopicExistsException;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class Kafka {
@@ -51,10 +63,9 @@ public class Kafka {
 
 		String time = (new Timestamp(System.currentTimeMillis())).toString().substring(0, 19);
 
-		System.out.println("[" + time + "] " + o.toString());
+		System.out.println("[" + time + "] " + (o == null ? null : o.toString()));
 
 	}
-
 	/**
 	 * 测试
 	 * 
@@ -66,7 +77,9 @@ public class Kafka {
 
 		String topic = "test_topic_888";
 
-		RecordMetadata meta = kf.syncSend(topic, topic + " send test " + new java.util.Date());
+		log("发送：");
+
+		RecordMetadata meta = kf.write(topic, topic + " send test " + new java.util.Date());
 
 		if (meta == null) {
 
@@ -80,19 +93,69 @@ public class Kafka {
 
 		}
 
-	}
+		log("接收：");
 
-	public KafkaProducer<Integer, String> producer = null;
+		ConsumerRecords<Integer, String> records = kf.read(topic);
+
+		if (records == null) {
+
+			log(records);
+
+		} else {
+
+			for (ConsumerRecord<Integer, String> record : records) {
+
+				log(record);
+
+			}
+
+		}
+
+	}
 
 	/**
 	 * 构造方法
 	 */
 	private Kafka() {
 
-		producer = createKafkaProducer();
+	}
+
+	/**
+	 * 实例化消费者
+	 * 
+	 * @return
+	 */
+	public KafkaConsumer<Integer, String> createKafkaConsumer() {
+
+		Properties props = new Properties();
+
+		// bootstrap server config is required for consumer to connect to brokers
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
+
+		// client id is not required, but it's good to track the source of requests beyond just ip/port
+		// by allowing a logical application name to be included in server-side request logging
+		props.put(ConsumerConfig.CLIENT_ID_CONFIG, "client-" + UUID.randomUUID());
+
+		// consumer group id is required when we use subscribe(topics) for group management
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-" + UUID.randomUUID());
+
+		// key and value are just byte arrays, so we need to set appropriate deserializers
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+		// // sets the reset offset policy in case of invalid or no offset
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+		return new KafkaConsumer<>(props);
 
 	}
 
+	/**
+	 * 实例化生产者
+	 * 
+	 * @return
+	 */
 	public KafkaProducer<Integer, String> createKafkaProducer() {
 
 		Properties props = new Properties();
@@ -246,13 +309,81 @@ public class Kafka {
 	}
 
 	/**
+	 * 接收消息
+	 * 
+	 * @param topic
+	 * @return
+	 */
+	public ConsumerRecords<Integer, String> read(String topic) {
+
+		KafkaConsumer<Integer, String> consumer = createKafkaConsumer();
+
+		consumer.subscribe(singleton(topic));
+
+		log("Subscribed to: " + topic);
+
+		ConsumerRecords<Integer, String> records = null;
+
+		ConsumerRecords<Integer, String> max_records = null;
+
+		int max = 0;
+
+		try {
+
+			for (int i = 0; i < 8; i++) {
+
+				// if required, poll updates partition assignment and invokes the configured rebalance listener
+				// then tries to fetch records sequentially using the last committed offset or auto.offset.reset policy
+				// returns immediately if there are records or times out returning an empty record set
+				// the next poll must be called within session.timeout.ms to avoid group rebalance
+				records = consumer.poll(Duration.ofSeconds(1));
+
+				int size = records.count();
+
+				if (size > 0 && size == max) {
+
+					max_records = records;
+
+					break;
+
+				}
+
+				if (size > max) {
+
+					max_records = records;
+
+					max = size;
+
+				}
+
+				// TimeUnit.MILLISECONDS.sleep(1_000);
+
+			}
+
+		} catch (Exception ex) {
+
+			ex.printStackTrace();
+
+		} finally {
+
+			consumer.close();
+
+		}
+
+		return max_records;
+
+	}
+
+	/**
 	 * 发送消息
 	 * 
 	 * @param topic
 	 * @param message
 	 * @return
 	 */
-	public RecordMetadata syncSend(String topic, String message) {
+	public RecordMetadata write(String topic, String message) {
+
+		KafkaProducer<Integer, String> producer = createKafkaProducer();
 
 		try {
 
@@ -261,6 +392,10 @@ public class Kafka {
 		} catch (Exception ex) {
 
 			ex.printStackTrace();
+
+		} finally {
+
+			producer.close();
 
 		}
 
